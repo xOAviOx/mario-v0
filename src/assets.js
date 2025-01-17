9aa0805d86844397053bb3288e51740db613466a
xOAviOx <avishuklacode@gmail.com>
2025-01-17 19:07:36 +0530

    Added half the features

---
+++ src/assets.js
@@ -0,0 +1,27 @@
+function loadAssets() {
+  loadSprite("bigyoshi", "/examples/sprites/YOSHI.png");
+  loadSprite("bean", "/sprites/bean.png");
+  loadSprite("bag", "/sprites/bag.png");
+  loadSprite("ghosty", "/sprites/ghosty.png");
+  loadSprite("spike", "/sprites/spike.png");
+  loadSprite("grass", "/sprites/grass.png");
+  loadSprite("steel", "/sprites/steel.png");
+  loadSprite("prize", "/sprites/jumpy.png");
+  loadSprite("apple", "/sprites/apple.png");
+  loadSprite("portal", "/sprites/portal.png");
+  loadSprite("coin", "/sprites/coin.png");
+  loadSound("coin", "/examples/sounds/score.mp3");
+  loadSound("powerup", "/examples/sounds/powerup.mp3");
+  loadSound("blip", "/examples/sounds/blip.mp3");
+  loadSound("hit", "/examples/sounds/hit.mp3");
+  loadSound("portal", "/examples/sounds/portal.mp3");
+}
+
+function setGameGravity() {
+  setGravity(3200);
+}
+
+export const gameUtils = {
+  loadAssets,
+  setGameGravity,
+};
---
+++ src/stages.js
@@ -0,0 +1,120 @@
+import { patrol } from "./objFunc";
+// define some constants
+export const JUMP_FORCE = 1320;
+export const MOVE_SPEED = 480;
+export const FALL_DEATH = 2400;
+
+export const LEVELS = [
+  [
+    "    0       ",
+    "   --       ",
+    "       $$   ",
+    " %    ===   ",
+    "            ",
+    "   ^^  > = @",
+    "============",
+  ],
+  [
+    "                          $",
+    "                          $",
+    "                          $",
+    "                          $",
+    "                          $",
+    "           $$         =   $",
+    "  %      ====         =   $",
+    "                      =   $",
+    "                      =    ",
+    "       ^^      = >    =   @",
+    "===========================",
+  ],
+  [
+    "     $    $    $    $     $",
+    "     $    $    $    $     $",
+    "                           ",
+    "                           ",
+    "                           ",
+    "                           ",
+    "                           ",
+    " ^^^^>^^^^>^^^^>^^^^>^^^^^@",
+    "===========================",
+  ],
+];
+
+// define what each symbol means in the level graph
+export const levelConf = {
+  tileWidth: 64,
+  tileHeight: 64,
+  tiles: {
+    "=": () => [
+      sprite("grass"),
+      area(),
+      body({ isStatic: true }),
+      anchor("bot"),
+      offscreen({ hide: true }),
+      "platform",
+    ],
+    "-": () => [
+      sprite("steel"),
+      area(),
+      body({ isStatic: true }),
+      offscreen({ hide: true }),
+      anchor("bot"),
+    ],
+    0: () => [
+      sprite("bag"),
+      area(),
+      body({ isStatic: true }),
+      offscreen({ hide: true }),
+      anchor("bot"),
+    ],
+    $: () => [
+      sprite("coin"),
+      area(),
+      pos(0, -9),
+      anchor("bot"),
+      offscreen({ hide: true }),
+      "coin",
+    ],
+    "%": () => [
+      sprite("prize"),
+      area(),
+      body({ isStatic: true }),
+      anchor("bot"),
+      offscreen({ hide: true }),
+      "prize",
+    ],
+    "^": () => [
+      sprite("spike"),
+      area(),
+      body({ isStatic: true }),
+      anchor("bot"),
+      offscreen({ hide: true }),
+      "danger",
+    ],
+    "#": () => [
+      sprite("apple"),
+      area(),
+      anchor("bot"),
+      body(),
+      offscreen({ hide: true }),
+      "apple",
+    ],
+    ">": () => [
+      sprite("ghosty"),
+      area(),
+      anchor("bot"),
+      body(),
+      patrol(),
+      offscreen({ hide: true }),
+      "enemy",
+    ],
+    "@": () => [
+      sprite("portal"),
+      area({ scale: 0.5 }),
+      anchor("bot"),
+      pos(0, -12),
+      offscreen({ hide: true }),
+      "portal",
+    ],
+  },
+};
