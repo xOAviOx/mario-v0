25ff1b7f14399f081b99e53f76d59e1ea87f7c1a
xOAviOx <avishuklacode@gmail.com>
2025-01-17 19:10:17 +0530

    object functions

--- src/main.js
+++ src/main.js
@@ -1,54 +1,19 @@
 import kaplay from "kaplay";
 import { gameUtils } from "./assets.js";
-
+import { big } from "./objFunc";
 import { JUMP_FORCE,FALL_DEATH, LEVELS, MOVE_SPEED, levelConf } from "./stages.js";
 
 kaplay({
   background: [141, 183, 255],
 });
 
+//game stage and gravity
 gameUtils.loadAssets();
 gameUtils.setGameGravity();
 
 
 
 
-// custom component that makes stuff grow big
-function big() {
-  let timer = 0;
-  let isBig = false;
-  let destScale = 1;
-  return {
-    // component id / name
-    id: "big",
-    // it requires the scale component
-    require: ["scale"],
-    // this runs every frame
-    update() {
-      if (isBig) {
-        timer -= dt();
-        if (timer <= 0) {
-          this.smallify();
-        }
-      }
-      this.scale = this.scale.lerp(vec2(destScale), dt() * 6);
-    },
-    // custom methods
-    isBig() {
-      return isBig;
-    },
-    smallify() {
-      destScale = 1;
-      timer = 0;
-      isBig = false;
-    },
-    biggify(time) {
-      destScale = 2;
-      timer = time;
-      isBig = true;
-    },
-  };
-}
 
 scene("game", ({ levelId, coins } = { levelId: 0, coins: 0 }) => {
   // add level to scene
--- src/objFunc.js
+++ src/objFunc.js
@@ -15,3 +15,42 @@ export function patrol(speed = 60, dir = 1) {
     },
   };
 }
+
+
+export 
+// custom component that makes stuff grow big
+function big() {
+  let timer = 0;
+  let isBig = false;
+  let destScale = 1;
+  return {
+    // component id / name
+    id: "big",
+    // it requires the scale component
+    require: ["scale"],
+    // this runs every frame
+    update() {
+      if (isBig) {
+        timer -= dt();
+        if (timer <= 0) {
+          this.smallify();
+        }
+      }
+      this.scale = this.scale.lerp(vec2(destScale), dt() * 6);
+    },
+    // custom methods
+    isBig() {
+      return isBig;
+    },
+    smallify() {
+      destScale = 1;
+      timer = 0;
+      isBig = false;
+    },
+    biggify(time) {
+      destScale = 2;
+      timer = time;
+      isBig = true;
+    },
+  };
+}
